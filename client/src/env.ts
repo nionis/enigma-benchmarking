d5dae9b8dbc0dbba2ec91bf438a97f1dc7e1cd63
nionis <stivennoni77@gmail.com>
2019-09-26 22:24:58 +0300

    update README.md

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,4 +1,4 @@
 export const enigmaContractAddress =
   "0x88987af7d35eabcad95915b93bfd3d2bc3308f06b7197478b0dfca268f0497dc";
 
-export const enigmaUrl = "http://192.168.17.84:3346";
+export const enigmaUrl = "http://172.18.87.150:3346";
